3bcbe4f3cdb2d1743814804699d64975f0d3140f
shwethaj1104 <shwethaj1104@gmail.com>
2023-03-28 16:25:17 +0530

    added side navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AfterViewInit, ChangeDetectorRef, Component,ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'NewsApp';
+  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+  // ChangeDetectorRef -->error in console saying state from true to false not detected
+  constructor(private observer : BreakpointObserver, private cd : ChangeDetectorRef, private newsApi : NewsService){
+
+  }
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    this.observer.observe(['(max-width:800px)'])
+    .subscribe((res)=>{
+      if(res?.matches){
+        this.sideNav.mode="over";
+        this.sideNav.close();
+      }else{
+        this.sideNav.mode = 'side';
+        this.sideNav.open();
+      }
+    })
+    this.cd.detectChanges();//here detecting chnages
+  }
+  searchSource(source:any){
+    this.newsApi.getArticlesByID(source.id)
+    .subscribe((res:any)=>{
+      this.selectedNewsChannel = source.name
+      this.articles = res.articles;
+    })
+  }
 }
